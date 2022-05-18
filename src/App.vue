db148a22fe7d62204aac5fbbcdedfac3c33337c6
Buechner, Simon <simon.buechner@davero.de>
2022-05-19 01:02:58 +0200

    update: change title main page

--- src/App.vue
+++ src/App.vue
@@ -14,7 +14,7 @@ import HelloWorld from "@/components/HelloWorld.vue";
     />
 
     <div class="wrapper">
-      <HelloWorld msg="You did it trice!" />
+      <HelloWorld msg="No Lets change something!" />
 
       <nav>
         <RouterLink to="/">Home</RouterLink>
